a16845c7524624d510051dcb38227502d574fffb
Marcin Lazar <mlazar@inelo.pl>
2024-11-05 19:57:25 +0100

    ange

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ const opts = {
       // https://peter.sh/experiments/chromium-command-line-switches/
       '--force-device-scale-factor=1', // Overrides the device scale factor for the browser UI and the contents.
       '--force-color-profile=srgb', // Force all monitors to be treated as though they have the specified color profile.
-      '--use-gl=egl',
+      '--use-angle',
       '--enable-webgl',
       '--enable-gpu',
       '--enable-webgl-image-chromium',
