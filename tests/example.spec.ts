8a64a3f5c791f682a131ed9ef2c1e5920bf7a1c0
Marcin Lazar <mlazar@inelo.pl>
2024-11-05 19:59:13 +0100

    screenshot tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -11,38 +11,38 @@ test.describe("Map Test", () => {
     await expect(hondurasLocator).toBeVisibleOnMap();
   });
 
-  // test("map should display popup after click on Honduras label", async ({ mapLocator, page }) => {
-  //   const hondurasCountryLocator = mapLocator(`map[id=mainMap] layer[id=countries-fill] filter["all", ["==", ["get", "fid"], 136]]`);
-  //   const hondurasLabelLocator = mapLocator(`map[id=mainMap] layer[id=countries-label] filter["all", ["==", ["get", "ABBREV"], "Hond."]]`);
+  test("map should display popup after click on Honduras label", async ({ mapLocator, page }) => {
+    const hondurasCountryLocator = mapLocator(`map[id=mainMap] layer[id=countries-fill] filter["all", ["==", ["get", "fid"], 136]]`);
+    const hondurasLabelLocator = mapLocator(`map[id=mainMap] layer[id=countries-label] filter["all", ["==", ["get", "ABBREV"], "Hond."]]`);
 
-  //   //Fit map to Honduras country fill
-  //   await hondurasCountryLocator.fitMap();
-  //   await hondurasLabelLocator.click();
+    //Fit map to Honduras country fill
+    await hondurasCountryLocator.fitMap();
+    await hondurasLabelLocator.click();
 
-  //   await expect(page.locator(".info-popup__country")).toContainText("Honduras");
-  // });
+    await expect(page.locator(".info-popup__country")).toContainText("Honduras");
+  });
 
-  // test("map should display country labels", async ({ mapLocator }) => {
-  //   const hondurasLocator = mapLocator(`map[id=mainMap] layer[id=countries-fill] filter["all", ["==", ["get", "fid"], 136]]`);
+  test("map should display country labels", async ({ mapLocator }) => {
+    const hondurasLocator = mapLocator(`map[id=mainMap] layer[id=countries-fill] filter["all", ["==", ["get", "fid"], 136]]`);
 
-  //   await hondurasLocator.fitMap({ padding: { left: 50, right: 50, top: 50, bottom: 50} });
+    await hondurasLocator.fitMap({ padding: { left: 50, right: 50, top: 50, bottom: 50} });
 
-  //   const countryDots = mapLocator(`map[id=mainMap] layer[id=countries-label]`).merge(); //merge all elements bbox;
+    const countryDots = mapLocator(`map[id=mainMap] layer[id=countries-label]`).merge(); //merge all elements bbox;
 
-  //   await expect(await countryDots.screnshoot({ expose: { backgroundColor: "red" } })).toMatchSnapshot("country-label.png");
-  // });
+    await expect(await countryDots.screnshoot({ expose: { backgroundColor: "red" } })).toMatchSnapshot("country-label.png");
+  });
 
-  // test("map should display cities icons after button clicked", async ({ mapLocator, mapController, page }) => {
-  //   const citiesIcons = mapLocator(`map[id=mainMap] layer[id=center-dot]`);
-  //   const controller = mapController('mainMap');
+  test("map should display cities icons after button clicked", async ({ mapLocator, mapController, page }) => {
+    const citiesIcons = mapLocator(`map[id=mainMap] layer[id=center-dot]`);
+    const controller = mapController('mainMap');
 
-  //   await controller.waitToMapStable();
+    await controller.waitToMapStable();
     
-  //   await Promise.all([
-  //     controller.waitToMapRepaint(),
-  //     page.locator('#showDotButton').click()
-  //   ]);
+    await Promise.all([
+      controller.waitToMapRepaint(),
+      page.locator('#showDotButton').click()
+    ]);
 
-  //   await expect(await citiesIcons.screnshoot({ expose: { backgroundColor: "blue" }, padding: 20 })).toMatchSnapshot("display-dot.png");
-  // });
+    await expect(await citiesIcons.screnshoot({ expose: { backgroundColor: "blue" }, padding: 20 })).toMatchSnapshot("display-dot.png");
+  });
 });
